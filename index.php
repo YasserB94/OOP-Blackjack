f010e114bd6ab38d0127f8c1ff53529061457a59
Yasser Bal <yasserbal@icloud.com>
2022-06-27 14:35:26 +0200

    Update with logic to load correct content

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 declare(strict_types=1);
+//START SESSION TO KEEP GAME
+if(!isset($_SESSION)){
+    session_start();
+}
+//REQUIRE BLACKJACK CLASS
+require_once __DIR__ . "/Classes/Blackjack.php";
+
+//REQUIRE PAGE HEAD - DOCTYPE/HEAD/META/HEADER
 require_once __DIR__ . "/Partials/head.php";
-require_once __DIR__ . "/Partials/body.php";
+
+//--------LOAD PAGE CONTENT--------
+//IF GAME WAS NOT STARTED && GAME IS NOT RUNNING
+if(!isset($_POST['startgame']) && !isset($_SESSION['game'])){
+require_once __DIR__ . "/Partials/rules.php";
+
+//IF GAME WAS STARTED
+}else if(isset($_POST['startgame'])){
+    //CREATE NEW GAME
+    $_SESSION['game'] = new Blackjack();
+    //REQUIRE GAME VIEW
+    require_once __DIR__ . "/Partials/blackjack_view.php";
+}else if(isset($_SESSION['game'])){
+    //IF THERE IS AN ACTIVE GAME
+    require_once __DIR__ . "/Partials/blackjack_view.php";
+}
+
+//LOAD PAGE TAIL = FOOTER//SCRIPTTAGS//ENDTAGS
 require_once __DIR__ . "/Partials/tail.php";
 ?>
