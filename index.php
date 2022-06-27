47998def24b364f65427b069e0cbdfeda630a3b6
Yasser Bal <yasserbal@icloud.com>
2022-06-27 10:32:28 +0200

    Proper Require

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 declare(strict_types=1);
-require __DIR__ . "/Partials/head.php";
-require __DIR__ . "/Partials/body.php";
-require __DIR__ . "/Partials/tail.php";
-
-
+require_once __DIR__ . "/Partials/head.php";
+require_once __DIR__ . "/Partials/body.php";
+require_once __DIR__ . "/Partials/tail.php";
 ?>
