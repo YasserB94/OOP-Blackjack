4d914c269e1e16237ed939d4c8adbe1c66475483
Yasser Bal <yasserbal@icloud.com>
2022-06-27 09:52:53 +0200

    Added Partials

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-echo 'Hello World!';
+declare(strict_types=1);
+require __DIR__ . "/Partials/head.php";
+require __DIR__ . "/Partials/body.php";
+require __DIR__ . "/Partials/tail.php";
+
 
 ?>
