2d73d999a59299963169d876b88ac13d3c7084eb
Yasser Bal <yasserbal@icloud.com>
2022-06-27 14:34:37 +0200

    Removed code that was not part of the game

--- Partials/blackjack_view.php
+++ Partials/blackjack_view.php
@@ -1,9 +1,18 @@
 <?php
 declare(strict_types=1);
+require_once __DIR__ . '/../Classes/Blackjack.php';
+$blackjack = new Blackjack();
 ?>
-
-    
-<div class="text-center m-5">
-    <h1>Hello World!</h1>
+<div class="border border-dark border-5 rounded p-2 m-5">
+<div class="text-center mx-auto my-5" style="width:80%">
+<h1>It's okay to give up!</h1>
+</div>
+<div class="text-center mx-auto mt-5" style="width:50%">
+    <form action="<?=htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    <input type="submit" value="Hit" name="hit" class="btn btn-warning" style="width:30%"></input>
+    <input type="submit" value="Stand" name ="stand" class="btn btn-success" style="width:30%"></input>
+    <input type="submit" value="Surrender" name="surrender"class="btn btn-danger" style="width:30%"></input>
+</form>
 </div>
 
+</div>
