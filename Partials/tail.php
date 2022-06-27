c03c0672785d979efd8a25cfab01b81a0e98c783
Yasser Bal <yasserbal@icloud.com>
2022-06-27 09:52:19 +0200

    Split index.php in partials - Implemented Bootstrap

---
+++ Partials/tail.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);?>
+<!-- Close Bootstrap container-->
+</div>
+<!-- BOOTSTRAP JS -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
