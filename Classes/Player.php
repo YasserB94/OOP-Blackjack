0e231078fa0b9c495bb3073b990a1c095f5ef604
Yasser Bal <yasserbal@icloud.com>
2022-06-27 11:16:59 +0200

    Implement exercise props/methods for player/dealer

---
+++ Classes/Player.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+class Player
+{
+    //Properties
+    protected array $cards;
+    protected bool $lost;
+    
+    function __construct(Deck $deck){
+        //Draw starting hand
+        $this->cards = $deck->drawCard();
+        $this->cards = $deck->drawCard();
+
+    }
+    function hit(){
+        //Add a card
+        $this->cards = $deck->drawCard();
+        //If new score is too high, surrender
+        if(getScore()>21){
+            $this->surrender();
+        }
+    }
+    function surrender(){
+        $this->lost = true;
+    }
+    function getScore(){
+        //Loop over cards and make sum of card values
+        $score;
+        foreach($this->cards as $key => $value){
+            $score+=$value->getValue();
+        }
+        return $score;
+    }
+    function hasLost(){
+        return $this->lost;
+    }
+}
+class Dealer extends Player{
+    function __construct(Deck $deck){
+        parent::__construct($deck);
+    }
+    function hit(){
+        parent::hit();
+        //Keep drawing untill score >=15
+        if(parent::getScore()<15){
+            $this->hit();
+        }
+    }
+}
+?>
